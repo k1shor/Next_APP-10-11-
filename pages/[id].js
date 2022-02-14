855336c6a0f401d69b83d5e454e22211200ba44c
kishor <k1shor@hotmail.com>
2022-02-14 10:25:43 +0545

    feb 14

--- pages/[id].js
+++ pages/[id].js
@@ -1,36 +1,45 @@
-//to get path
+import Head from 'next/head'
+import Link from 'next/link'
 
-export const getStaticPaths = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const userdata = await response.json()
+export const getStaticPaths=async()=>{
+    const response=await fetch('https://jsonplaceholder.typicode.com/users')
+    const userdata= await response.json()
 
-    const paths = userdata.map((user)=>{
-        return {
-            params: {id:user.id.toString()}
+    const paths=userdata.map(data=>{
+        return{
+            params:{id:data.id.toString()}
         }
     })
-    return {
-        paths,
-        fallback:false}
-}
-
-
-export const getStaticProps = async(context) =>{
-    const id = context.params.id
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-    const userdata = await response.json()
-
-    return {
-        props:{user:userdata}
+    return{
+        paths, 
+        fallback:false
     }
 }
 
-const fetchuserdata = ({userdetails}) => {
+export const getStaticProps=async(context)=>{
+    const id=context.params.id
+    const response=await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const userdata= await response.json()
+
+    return{
+        props:{userinfo:userdata}
+    }
+}
+
+const userDetails = ({userinfo}) => {
     return ( 
         <>
-        <h1>{userdetails.username}</h1>
+
+
+        <h1>{userinfo.name}</h1>
+        <h2>{userinfo.username}</h2>
+        <h2>{userinfo.email}</h2>
+        {/* <h3>{userinfo.address.city}</h3> */}
+        <p>{userinfo.phone}, {userinfo.website}</p>
+
+
         </>
      );
 }
  
-export default fetchuserdata;
+export default userDetails;
